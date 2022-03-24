31a2a98191e5dfd365d5ab5fd4de6fe6f6954e7f
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-03-25 02:12:04 +0300

    patch:adding modified book.js

--- js/book.js
+++ js/book.js
@@ -66,21 +66,6 @@ Fare.prototype.getTotalAmount=function(){
 
 
 };  
->>>>>>> ft-carDetails
 
-
-
-
-const destination = document.querySelector("#destination");
-const cost = document.querySelector("#cost");
-
-destination.addEventListener("submit", showCost);
-function showCost(ev) {
-    ev.preventDefault();
-    function randomPriceRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    cost.innerHTML =`Trip booked! Pay Ksh.${randomPriceRange(250,500)}`;
-}
 
 
